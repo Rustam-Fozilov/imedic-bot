18d62d2471a5ed9a94ef8573d99e11dd149d2df2
Rustam Fozilov <fozilovrustam32@gmail.com>
2024-09-26 20:00:37 +0500

    add: auth for telescope

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -42,9 +42,5 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
-
-        Fortify::authenticateUsing(function (Request $request) {
-            dd('auth');
-        });
     }
 }
